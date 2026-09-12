e696cef6f27805ac215b851a51d5b6a31cec6990
Ryan Fox <rfox75@gmail.com>
2026-09-12 14:01:00 -0600

    Add complete Sidetrack static build and .nojekyll for GitHub Pages

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
 const CACHE = 'sidetrack-v1';
+
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))),
   );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
