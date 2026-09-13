2363d5a328cd735204754f0dafcc6cdb2ceab7bf
Ryan Fox <rfox75@gmail.com>
2026-09-13 00:48:03 -0600

    HUD pass: opaque two-row chrome, louder cargo badges.

--- assets/boot.js
+++ assets/boot.js
@@ -1,5 +1,5 @@
 const parts = await Promise.all(
-  ['./index-Dl4mKFr5.1.js', './index-Dl4mKFr5.2.js'].map((u) =>
+  ['./index-C_FrWyP2.1.js', './index-C_FrWyP2.2.js'].map((u) =>
     fetch(new URL(u, import.meta.url)).then((r) => {
       if (!r.ok) throw new Error('missing ' + u);
       return r.text();
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sidetrack-v6';
+const CACHE = 'sidetrack-v7';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
