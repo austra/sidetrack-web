e401ec0f4eb8473db0ae24ac6c046dab05ebb3e5
Ryan Fox <rfox75@gmail.com>
2026-09-13 01:00:35 -0600

    Belt kind stamps and readable clock (shell).

--- assets/boot.js
+++ assets/boot.js
@@ -1,5 +1,5 @@
 const parts = await Promise.all(
-  ['./index-C_FrWyP2.1.js', './index-C_FrWyP2.2.js'].map((u) =>
+  ['./index-CAwJHW5K.1.js', './index-CAwJHW5K.2.js'].map((u) =>
     fetch(new URL(u, import.meta.url)).then((r) => {
       if (!r.ok) throw new Error('missing ' + u);
       return r.text();
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sidetrack-v7';
+const CACHE = 'sidetrack-v8';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
