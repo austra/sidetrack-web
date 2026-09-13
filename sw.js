de6f239dc6e54cec9cd1d5882cd665eefc38290c
Ryan Fox <rfox75@gmail.com>
2026-09-12 20:05:51 -0600

    Polish: hard car rules, Rules modal, toggleable BGM

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sidetrack-v1';
+const CACHE = 'sidetrack-v3';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
