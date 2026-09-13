58d29be1d372488d3fc0ee0b774d2851aeb19f8c
Ryan Fox <rfox75@gmail.com>
2026-09-13 08:10:30 -0600

    Sidetrack v14 shell — waybill inline flex, no leftover dismiss.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sidetrack-v13';
+const CACHE = 'sidetrack-v14';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
