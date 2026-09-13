1354402aa080d6462d703793b8e69cefba450087
Ryan Fox <rfox75@gmail.com>
2026-09-13 02:33:48 -0600

    Sidetrack v12 shell — waybill stays until Lock consist.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sidetrack-v11';
+const CACHE = 'sidetrack-v12';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
