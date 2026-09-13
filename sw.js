381eb0e50a3fbfca2228aa4f2bb91dcfd419f41c
Ryan Fox <rfox75@gmail.com>
2026-09-13 00:27:17 -0600

    Ship waybill, consist, Design HUD pass.

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
-PLACEHOLDER_LOAD_FROM_DISK
+const CACHE = 'sidetrack-v6';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((hit) => {
+      return (
+        hit ||
+        fetch(event.request)
+          .then((res) => {
+            const copy = res.clone();
+            if (res.ok && new URL(event.request.url).origin === self.location.origin) {
+              caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+            }
+            return res;
+          })
+          .catch(() => caches.match('./index.html'))
+      );
+    }),
+  );
+});
