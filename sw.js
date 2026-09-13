8ab0a09ff7eaf0ee73d4d433a637e5e10a15cba4
Ryan Fox <rfox75@gmail.com>
2026-09-13 09:08:03 -0600

    Sidetrack v15 shell — results CTAs pinned, Reset career.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sidetrack-v14';
+const CACHE = 'sidetrack-v15';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html'])));
