1fed9bb5088af9aeac3c0a0223b5fadcf5463d54
Ryan Fox <rfox75@gmail.com>
2026-09-13 14:19:22 -0600

    v20 shell: station signatures, SW sidetrack-v20, boot DYFq_t4a

--- assets/boot.js
+++ assets/boot.js
@@ -1,5 +1,5 @@
 const parts = await Promise.all(
-  ['./index-DJIJiCn6.1.js', './index-DJIJiCn6.2.js'].map((u) =>
+  ['./index-DYFq_t4a.1.js', './index-DYFq_t4a.2.js'].map((u) =>
     fetch(new URL(u, import.meta.url)).then((r) => {
       if (!r.ok) throw new Error('missing ' + u);
       return r.text();
