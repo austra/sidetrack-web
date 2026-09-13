855c25d5619c3e0dc15a0a54b4365f497ffd9249
Ryan Fox <rfox75@gmail.com>
2026-09-13 01:34:23 -0600

    Playtest batch 1: clock, fill, Coal remount (shell).

--- assets/boot.js
+++ assets/boot.js
@@ -1,5 +1,5 @@
 const parts = await Promise.all(
-  ['./index-CAwJHW5K.1.js', './index-CAwJHW5K.2.js'].map((u) =>
+  ['./index-CdSZWgUS.1.js', './index-CdSZWgUS.2.js'].map((u) =>
     fetch(new URL(u, import.meta.url)).then((r) => {
       if (!r.ok) throw new Error('missing ' + u);
       return r.text();
